03aae6abc66bdf93f0efd748cb502eea8b08718f
Meret6832 <meret.baumgartner@student.uva.nl>
2024-10-06 13:18:00 +0200

    test easy solutions EnergiBridge/target/release/energibridge --summary -o test.csv sleepMX wrong prompt!

--- correctnesstests/test_correctness.js
+++ correctnesstests/test_correctness.js
@@ -1,36 +1,51 @@
 const { readFile, writeFile } = require('fs/promises');
 
 writeFile("./results.csv", "", {flag: "w+"});
-["M-LN", "M-WJ"].forEach(problem => {
+["E-MX", "E-TT", "M-LN", "M-WJ"].forEach(problem => {
     let inOutFile = "./inputs-outputs/" + problem + ".json";
     readFile(inOutFile, "utf-8").then((data) => {
         const inOut = JSON.parse(data);
         ["ChatGPT", "Claude", "Gemini"].forEach(llm => {
             for (let r = 0; r < 3; r++) {
                 const fooPath = "../solutions/" + problem + "_" + llm + "_" + r.toString() + ".js";
-                const mod = require(fooPath);
-                let passed = true;
-                inOut.forEach(test => {
-                    let input = test[0];
-                    let expectedOutput = test[1];
-                    console.log(problem, input);
-                    try {
-                        var output = mod.foo(...input);
-                    } catch (error) {
-                        var output = mod.foo(input);
-                    }
-                    if (JSON.stringify(expectedOutput) != JSON.stringify(output)) {
-                        console.log("FALSE", expectedOutput, output)
-                        passed = false;
-                    }
-                })
-                writeFile("./results.csv", problem + "," + llm + "," + r.toString() + "," + passed.toString() + "\n", {flag: "a+"});
+                try {
+                    const mod = require(fooPath);
+                    let passed = true;
+                    inOut.forEach(test => {
+                        let input = test[0];
+                        let expectedOutput = test[1];
+
+                        const numParams = mod.foo.length;
+                        const paramsError = Error("got " + input.length + " params instead of " + numParams);
+                        try {
+                            if (Array.isArray(input) && input.length != numParams) {
+                                throw paramsError
+                            }
+                            var output = mod.foo(...input);
+                        } catch (error) {
+                            if (error == paramsError) {
+                                throw paramsError;
+                            } else {
+                                var output = mod.foo(input);
+                            }
+                        }
+                        if (JSON.stringify(expectedOutput) != JSON.stringify(output)) {
+                            console.log("FALSE", input, expectedOutput, output)
+                            passed = false;
+                        }
+                    })
+                    writeFile("./results.csv", problem + "," + llm + "," + r.toString() + "," + passed.toString() + "\n", {flag: "a+"});
+                }
+                catch (err) {
+                    writeFile("./results.csv", problem + "," + llm + "," + r.toString() + "," + false.toString() + "," + err.toString() + "\n", {flag: "a+"});
+                }
             }
         })
     })
-    .catch((err) => {
-        for (let r = 0; r < 3; r++) {
-            writeFile("./results.csv", problem + "," + llm + "," + r.toString() + ",ERROR" + "\n", {flag: "a+"});
-        }
-    })
+    // .catch((err) => {
+    //     throw err;
+    //     for (let r = 0; r < 3; r++) {
+    //         writeFile("./results.csv", problem + ",," + r.toString() + ",ERROR" + "\n", {flag: "a+"});
+    //     }
+    // })
 })
